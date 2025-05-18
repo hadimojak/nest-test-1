753f2265e38c82d4367267c475289960034fa794
hadi arbabi <hadimojak@gmail.com>
2025-05-18 15:37:56 +0330

    messages

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  constructor() {}
+
+  @Get()
+  listMessages() {}
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log({ body });
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log({ id });
+  }
+}
